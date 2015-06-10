3340aec32f98f4bac70116086695e01498eb8e5a
= <=>
2015-06-09 23:23:23 -0300

    Versão 0.8

--- PI_Data/pi.js
+++ PI_Data/pi.js
@@ -1,12 +1,58 @@
 function PiData() {
 
-    var piPlayer = this.$;
-    var element = this;
-    var youtube = element.youtube;
-    var player = piPlayer.player;
-    var controls = piPlayer.controls;
-    var progressBar = piPlayer.progressBar;
-    var progressIndicator = piPlayer.progressBarIndicator;
+    function piPlayerSingle(element) {
+        var playButton = element.$.play;
+        var pauseButton = element.$.pause;
+        var controls = element.$.controls;
+        var player = element.$.player;
+        this.layout = {};
+        this.layout.play = function(html) {
+            if (html !== undefined) {
+                playButton.innerHTML = html;
+            } else {
+                return playButton.innerHTML;
+            }
+        }
+        this.layout.pause = function(html) {
+            if (html !== undefined) {
+                pauseButton.innerHTML = html;
+            } else {
+                return pauseButton.innerHTML;
+            }
+        }
+        this.layout.controlOut = function(e) {
+            setTimeout(function() {
+                controls.style.display = "none";
+            }, 500);
+        }
+        this.layout.controlIn = function(e) {
+            controls.style.display = "Block";
+        }
+
+        this.play = function() {
+            player.play();
+            play.style.display = "none";
+            pause.style.display = "block";
+        }
+        this.pause = function() {
+            player.pause();
+            play.style.display = "block";
+            pause.style.display = "none";
+        }
+
+    }
+
+    var piPlayer = this.$,
+        element = this,
+        youtube = element.youtube,
+        player = piPlayer.player,
+        play = piPlayer.play,
+        pause = piPlayer.pause,
+        controls = piPlayer.controls,
+        buffer = piPlayer.buffer,
+        watched = piPlayer.watched,
+        progressBar = piPlayer.progressBar,
+        progressIndicator = piPlayer.progressBarIndicator;
 
     if (youtube === undefined || youtube == "") {
         console.error("Error 1, Please insert the video Code to use the element")
@@ -22,6 +68,22 @@ function PiData() {
         ironAjax.addEventListener("response", loadVideo, false);
 
     }
+
+    play.addEventListener("click", playVideo, false);
+    pause.addEventListener("click", pauseVideo, false);
+
+    function playVideo() {
+        player.play();
+        play.style.display = "none";
+        pause.style.display = "block";
+    }
+
+    function pauseVideo() {
+        player.pause();
+        play.style.display = "block";
+        pause.style.display = "none";
+    }
+
 
     function generateQualityData(list) {
         var mediumQ = [];
@@ -68,15 +130,21 @@ function PiData() {
     player.addEventListener("timeupdate", function(e) {
         var currentTime = player.currentTime;
         var duration = player.duration;
-        var percentage = currentTime * 100 / duration;
+        var percentage = (currentTime * 100) / duration;
         changePercentage(percentage);
+        var buffered = (player.buffered.end(0) * 100) / duration;
+        changeBuffer(buffered);
     });
+
+    function changeBuffer(percent) {
+        buffer.style.width = percent + "%";
+    }
 
     function changePercentage(percent) {
         if (!isDown) {
+            watched.style.width = percent + "%";
             var position = (percent * progressBar.offsetWidth) / 100;
-            progressIndicator.style.left = position + "px";
-
+            progressIndicator.style.left = position - 10 + "px";
             var percentage = (100 * position) / progressBar.offsetWidth;
             var progressPercent = Math.round(percentage);
             var event = new Event('pi-progress-changed');
@@ -85,9 +153,9 @@ function PiData() {
         }
     }
 
-    function getVideoTime(percentage){
+    function getVideoTime(percentage) {
         var duration = player.duration;
-        var time = duration * percentage/100;
+        var time = duration * percentage / 100;
         return time;
     }
 
@@ -124,8 +192,11 @@ function PiData() {
 
     progressBar.addEventListener("mousemove", mouseMove, false);
 
-    element.addEventListener("pi-progress-changed", function(e) {
-        console.log(e.progress);
-    });
+    this.control = new piPlayerSingle(this);
+
+    element.addEventListener("mouseout", this.control.layout.controlOut, true);
+
+    element.addEventListener("mousemove", this.control.layout.controlIn, true);
+
 
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,42 @@
-# PI Player #
+# PI Player v0.8 #
 
-Em Refatoração
+O π é um player interativo baseado no video element do HTML5 e no polyfill Polymer da Google, a ideia principal do π é oferecer todos os recursos do embed do YouTube mas te deixar livre para estilizar seu player como quiser.
+
+## Instalação ##
+Para ter o Pi em seu site você precisa baixar as dependencias do Polymer, Para isso baixe o projeto e entre na pasta do projeto
+em seguida use o comando
+``` $ bower install ```
+no terminal ou no CMD
+
+Após isso é só adicionar o script do **webcomponents.js** no seu projeto
+``` <script src="bower_components/webcomponentsjs/webcomponents-lite.js"></script> ```
+e em seguida importar o PI em sua aplicação
+``` <link rel="import" href="PI_Data/PI.html"> ```
+
+depois disso é só adicionar o player no seu código HTML, quantas vezes quiser
+
+``` <pi-player id="player1" youtube="ID_DO_VÍDEO"></pi-player> ```
+
+## A Pasta PI_Data ##
+A pasta PI_Data é onde fica armazenado todos os arquivos e componentes do PI,
+que são eles
+- PI.html
+- pi-theme.css
+- pi.js
+
+O PI.html é o componente em sí.
+O pi-theme.css é a folha de estilo do pi, nela você pode editar todos os componentes do player.
+O pi.js é os código de ações e de eventos do player, que faz com que ele possa pegar os dados do YouTube e mostrar na tela
+
+## JavaScript ##
+O Pi vem com algumas funções já pre-programadas, como a ideia é dar livre escolha ao desenvolvedor, tudo do player é manipulável, desde as labels dos botões até o estilo.
+Para ter acesso a esses comandos você deve utilizar o objeto ``` control ``` presente no elemento.
+esse objeto contém funções de controle do vídeo, como play e pause. e também contém funções de edição de layout.
+
+
+# ATENÇÃO #
+
+O Pi ainda está em faze de testes, então cuidado ao colocar no seu projeto.
+
+## Licença
+[MIT License](http://opensource.org/licenses/MIT)
